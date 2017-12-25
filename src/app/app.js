19eed54832209b74975700e74d1f51d967f70fd1
Dmytro Zhyrov <dzhyrov@actum.cz>
2017-12-25 17:12:32 +0100

    merry christmas

--- src/app/app.js
+++ src/app/app.js
@@ -8,10 +8,12 @@ import init from './init';
 import { render, renderFactory } from './render';
 import suffix from './components/suffix';
 import dropdown from './components/dropdown';
+import accordion from './components/accordion';
 
 const app = (config) => {
     init(suffix, document.querySelector('.js-suffix'));
     render(suffix, document.getElementsById('dropdown'));
+    render(suffix, document.getElementsById('accordion'));
 };
 
 app(window.config);
