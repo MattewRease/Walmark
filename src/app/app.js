45fa870efaf2f9848e205e064af41df74b2f5bbf
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-10 09:49:18 +0100

    js

--- src/app/app.js
+++ src/app/app.js
@@ -6,14 +6,11 @@ import 'babel-polyfill';
 import 'svgxuse';
 import init from './init';
 import { render, renderFactory } from './render';
-import suffix from './components/suffix';
 import dropdown from './components/dropdown';
 import accordion from './components/accordion';
 
 const app = (config) => {
-    init(suffix, document.querySelector('.js-suffix'));
-    render(suffix, document.getElementsById('dropdown'));
-    render(suffix, document.getElementsById('accordion'));
+    init(document.getElementById('dropdown'));
 };
 
 app(window.config);
