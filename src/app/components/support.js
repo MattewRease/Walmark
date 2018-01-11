544ce399db64943f30f8b23d6081acde38a4e392
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-11 18:02:55 +0100

    progressbar

--- src/app/components/support.js
+++ src/app/components/support.js
@@ -15,10 +15,16 @@ export default () => {
             icon.toggle('health-support__day--tick');
             icon.toggle('health-support__day--cross');
             innerSpan.innerHTML === 'Mark' ? innerSpan.innerHTML = 'Unmark' : innerSpan.innerHTML = 'Mark';
-
+            procent();
             for (let j = 0; j < btn.length; j += 1) {
                 iconAll[j].classList.toggle('disable');
             }
         });
     }
+
+    function procent() {
+        const done = document.querySelectorAll('.day--yes').length;
+        return document.querySelector('.progress-bar').style.width = (done * 20) + '%';
+    }
+    procent();
 };
