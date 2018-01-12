376af5d117fc4099c4d48953ce18333c86f413db
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-12 17:00:02 +0100

    scroll by weeks

--- src/app/components/weekNav.js
+++ src/app/components/weekNav.js
@@ -5,22 +5,28 @@ export default () => {
 
     nextBtn.addEventListener('click', scrollNext);
     prevBtn.addEventListener('click', scrollPrev);
+    calendar.onscroll = () => { weekNumber(); };
 
-    function scrollNext(e) {
-        if (calendar.scrollTop < 650) {
-            calendar.scrollBy({ top: 643, left: 0, behavior: 'smooth' });
-        } else {
-            calendar.scrollBy({ top: 633, left: 0, behavior: 'smooth' });
-
-        }
+    function scrollNext() {
+        calendar.scrollTop < 650 ? calendar.scrollBy({ top: 643, left: 0, behavior: 'smooth' }) : calendar.scrollBy({ top: 633, left: 0, behavior: 'smooth' });
     }
 
-    function scrollPrev(e) {
-        if (calendar.scrollTop < 650) {
-            calendar.scrollBy({ top: -643, left: 0, behavior: 'smooth' });
-        } else {
-            calendar.scrollBy({ top: -640, left: 0, behavior: 'smooth' });
+    function scrollPrev() {
+        calendar.scrollTop < 650 ? calendar.scrollBy({ top: -643, left: 0, behavior: 'smooth' }) : calendar.scrollBy({ top: -640, left: 0, behavior: 'smooth' });
+    }
+    document.getElementById('weekNumder').innerHTML = 'Week 1';
 
+    function weekNumber() {
+        if (calendar.scrollTop > 275 && calendar.scrollTop < 937) {
+            document.getElementById('weekNumder').innerHTML = 'Week 2';
+        } else if (calendar.scrollTop > 938 && calendar.scrollTop < 1594) {
+            document.getElementById('weekNumder').innerHTML = 'Week 3';
+        } else if (calendar.scrollTop > 1595 && calendar.scrollTop < 2034) {
+            document.getElementById('weekNumder').innerHTML = 'Week 4';
+        } else if (calendar.scrollTop > 2035) {
+            document.getElementById('weekNumder').innerHTML = 'Week 5';
+        } else if (calendar.scrollTop < 200) {
+            document.getElementById('weekNumder').innerHTML = 'Week 1';
         }
     }
 };
