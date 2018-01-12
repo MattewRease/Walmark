e3455cc1162603b238f2ab462237a6c0ff7401cd
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-12 16:11:17 +0100

    smooth scroll

--- src/app/components/weekNav.js
+++ src/app/components/weekNav.js
@@ -1,28 +1,25 @@
 export default () => {
     const prevBtn = document.querySelector('.health-support__navigation-prev');
     const nextBtn = document.querySelector('.health-support__navigation-next');
-    const days = document.querySelectorAll('.health-support__day');
     const calendar = document.querySelector('.health-support__week');
-    const week = calendar.querySelector('.targetDay');
-
 
     nextBtn.addEventListener('click', scrollNext);
     prevBtn.addEventListener('click', scrollPrev);
 
     function scrollNext(e) {
         if (calendar.scrollTop < 650) {
-            week.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            calendar.scrollBy({ top: 643, left: 0, behavior: 'smooth' });
         } else {
-            calendar.scrollTop += 633;
+            calendar.scrollBy({ top: 633, left: 0, behavior: 'smooth' });
 
         }
     }
 
     function scrollPrev(e) {
         if (calendar.scrollTop < 650) {
-            calendar.scrollTop -= 643;
+            calendar.scrollBy({ top: -643, left: 0, behavior: 'smooth' });
         } else {
-            calendar.scrollTop -= 643;
+            calendar.scrollBy({ top: -640, left: 0, behavior: 'smooth' });
 
         }
     }
