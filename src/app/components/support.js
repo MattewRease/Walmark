16faca32f51cb67c73f28ec1f065b2a24e4a3666
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-15 15:16:07 +0100

    deleted unused

--- src/app/components/support.js
+++ src/app/components/support.js
@@ -6,26 +6,15 @@ export default () => {
             const selected = btn[i].previousElementSibling;
             const innerSpan = btn[i].querySelector('span');
             const icon = selected.querySelector('svg').classList;
-            // const iconTick = selected.querySelector('.icon health-support__day--tick');
-            // const iconCross = selected.querySelector('.health-support__day--cross');
             const iconAll = selected.querySelectorAll('svg');
-
             selected.classList.toggle('day--no');
             selected.classList.toggle('day--yes');
-                    for (let j = 0; j < btn.length; j += 1) {
-                        iconAll[j].classList.toggle('disable');
-                        //     // if (iconAll[j].classList.contains('disable')) {
-                    //     //     iconAll[j].classList.remove('disable');
-                    //     // } else {
 
-                    //     //     iconAll[j].classList.add('disable');
-                    //     // }
-                    }
+            for (let j = 0; j < btn.length; j += 1) {
+                iconAll[j].classList.toggle('disable');
+                progressBar();
+            }
             innerSpan.innerHTML === 'Mark' ? innerSpan.innerHTML = 'Unmark' : innerSpan.innerHTML = 'Mark';
-            progressBar();
-            // iconTick.classList.toggle('disable');
-            // iconCross.classList.toggle('disable');
-
         });
     }
 
