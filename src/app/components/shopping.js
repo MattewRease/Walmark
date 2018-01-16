8b28abcae2b6e14fd962d1c6de2607c0c99e16f1
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-16 18:37:24 +0100

    possible add long programm

--- src/app/components/shopping.js
+++ src/app/components/shopping.js
@@ -3,7 +3,9 @@ export default () => {
     const trashBtn = document.querySelector('.shopping-list__trash-icon');
     const item = document.querySelector('.shopping-list__item');
     const title = document.querySelector('.program-length__list--title');
+    const titleLong = document.querySelector('.program-length__list-long--title');
     const list = document.querySelector('.program-length__list-short');
+    const listLong = document.querySelector('.program-length__list-long');
 
     shoppingList.onclick = () => {
         document.querySelector('.shopping-list').classList.add('active');
@@ -14,6 +16,16 @@ export default () => {
     };
 
     document.querySelector('[data-program="short"]').onclick = () => {
+        addToShop();
+        addContentShort();
+    };
+
+    document.querySelector('[data-program="long"]').onclick = () => {
+        addToShop();
+        addContentLong();
+    };
+
+    function addToShop() {
         const copyItem = item.cloneNode(true);
         const newItem = item.parentNode.insertBefore(copyItem, item.nextSibling);
         newItem.className = 'shopping-list__item--new';
@@ -26,14 +38,32 @@ export default () => {
             false;
         }
 
-        addContent();
         notify();
-    };
+    }
 
-    function addContent() {
+    function addContentShort() {
         const newItem = document.querySelector('.shopping-list__item--new');
         const copyList = list.cloneNode(true);
         newItem.childNodes[1].innerHTML += title.textContent;
+        newItem.childNodes[5].appendChild(copyList);
+
+        document.querySelector('.shopping-list__item--new').onclick = (event) => {
+            const newItem = document.querySelector('.shopping-list__item--new');
+            let target = event.target;
+            while (target !== this) {
+                if (target.tagName === 'svg') {
+                    target.parentNode.parentNode.parentNode.removeChild(target.parentNode.parentNode);
+                    return;
+                }
+                target = target.parentNode;
+            }
+        };
+    }
+
+    function addContentLong() {
+        const newItem = document.querySelector('.shopping-list__item--new');
+        const copyList = listLong.cloneNode(true);
+        newItem.childNodes[1].innerHTML += titleLong.textContent;
         newItem.childNodes[5].appendChild(copyList);
 
         document.querySelector('.shopping-list__item--new').onclick = (event) => {
@@ -62,7 +92,6 @@ export default () => {
                 note.innerHTML = 'Program added to your shopping list!';
             }, 300);
             setTimeout(() => {
-                console.log(noteWrapp);
                 noteWrapp.removeChild(noteWrapp.lastChild);
             }, 3000);
         }
