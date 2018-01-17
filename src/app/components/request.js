b6defbe68e6d3fbf52909515392cc8d56b3fbede
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-17 17:42:29 +0100

    noties added, stiil bad inpl..

--- src/app/components/request.js
+++ src/app/components/request.js
@@ -1,27 +1,32 @@
 import axios from 'axios';
-import { task } from './support';
 
 export default () => {
     const APIUrl = 'http://localhost:5003/reminders';
     const day = document.querySelector('[data-reminder="day"]');
     const time = document.querySelector('[data-reminder="time"]');
     const setBtn = document.querySelector('[data-reminder="set"]');
-    let selectedDay;
-    let selectedTime;
+    setBtn.disabled = true;
+    let selectedDay = 'Select day';
+    let selectedTime = 'Select time';
 
-    day.addEventListener('change', () => {
+    day.addEventListener('change', (event) => {
         const selectEl = event.target;
         selectedDay = selectEl.value;
+        (selectedDay !== 'Select day') && (selectedTime !== 'Select time') ? setBtn.disabled = false : setBtn.disabled = true;
     });
 
-    time.addEventListener('change', () => {
+    time.addEventListener('change', (event) => {
         const selectEl = event.target;
         selectedTime = selectEl.value;
+        (selectedDay !== 'Select day') && (selectedTime !== 'Select time') ? setBtn.disabled = false : setBtn.disabled = true;
     });
 
     setBtn.addEventListener('click', postRes);
 
     function postRes() {
+        const noteWrapp = document.createElement('div');
+        document.body.appendChild(noteWrapp);
+        noteWrapp.className = 'note-wrap';
         axios({
             method: 'post',
             url: APIUrl,
@@ -30,5 +35,20 @@ export default () => {
                 time: selectedTime
             }
         });
+        notify();
+    }
+
+    function notify() {
+        const noteWrapp = document.querySelector('.note-wrap');
+        const note = document.createElement('p');
+        noteWrapp.appendChild(note);
+        note.className = 'note info';
+        setTimeout(() => {
+            note.classList.add('active');
+            note.innerHTML = 'Reminder set!';
+        }, 300);
+        setTimeout(() => {
+            noteWrapp.removeChild(noteWrapp.firstChild);
+        }, 3000);
     }
 };
