2ace4537683f8bb615f83617477a648bd949785e
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-18 12:01:43 +0100

    still shit

--- src/app/components/rateProduct.js
+++ src/app/components/rateProduct.js
@@ -17,17 +17,59 @@ export default () => {
     }
 
     rateBtn.addEventListener('click', () => {
+        const noteWrapp = document.createElement('div');
+        document.body.appendChild(noteWrapp);
+        noteWrapp.className = 'note-wrap';
         const review = {
             rate: selectedStar,
             text: reviewText.value
         };
 
-        axios({
-            method: 'post',
-            url: APIUrl,
-            data: {
-                review
-            }
-        });
+        if (review.rate !== '' && review.text !== '') {
+            axios({
+                method: 'post',
+                url: APIUrl,
+                data: {
+                    review
+                }
+            });
+            notify();
+        } else {
+            notifyWarning();
+        }
+        reviewText.value = '';
+        function notify() {
+            const noteWrapp = document.querySelector('.note-wrap');
+            const note = document.createElement('p');
+            const newNote = document.querySelector('.note');
+
+            newNote ? noteWrapp.insertBefore(note, newNote) : noteWrapp.appendChild(note);
+
+            note.className = 'note info';
+            setTimeout(() => {
+                note.classList.add('active');
+                note.innerHTML = 'Your review has been sent!';
+            }, 300);
+            setTimeout(() => {
+                noteWrapp.removeChild(noteWrapp.firstChild);
+            }, 3000);
+        }
+
+        function notifyWarning() {
+            const noteWrapp = document.querySelector('.note-wrap');
+            const note = document.createElement('p');
+            const newNote = document.querySelector('.note');
+
+            newNote ? noteWrapp.insertBefore(note, newNote) : noteWrapp.appendChild(note);
+
+            note.className = 'note warning';
+            setTimeout(() => {
+                note.classList.add('active');
+                note.innerHTML = 'Failed! Fill the Form!';
+            }, 300);
+            setTimeout(() => {
+                noteWrapp.removeChild(noteWrapp.firstChild);
+            }, 3000);
+        }
     });
 };
