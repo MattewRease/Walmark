a4266e219865009ee2f9bda156fd67ed55c9685a
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-22 09:24:12 +0100

    js part

--- src/app/components/progress.js
+++ src/app/components/progress.js
@@ -20,3 +20,28 @@ export default () => {
         }
     }
 };
+
+// export default class Progress {
+//     constructor(container) {
+//         this.container = container;
+//         this.rate = container.getElementById('rateIndex');
+//         this.arrow = container.getElementById('rateArrow');
+//         this.bg = container.getElementById('rateBg');
+//         this.rateChange = `rotate(${rate.dataset.index * 1.8}deg)`;
+//         if (rate.dataset.index < 0 || rate.dataset.index > 100) {
+//             alert('bad range');
+//         } else {
+//             rate.innerHTML = (rate.dataset.index);
+//             arrow.style.transform = rateChange;
+//             bg.style.transform = rateChange;
+
+//             if (rate.dataset.index < 31) {
+//                 rate.classList.add('range--low');
+//             } else if (rate.dataset.index < 70) {
+//                 rate.classList.add('range--middle');
+//             } else {
+//                 rate.classList.add('range--hight');
+//             }
+//         }
+//     }
+// }
