8b4867b470f4251818329dc51731b244579613cf
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-24 11:23:51 +0100

    classes dropdown

--- src/app/components/dropdown.js
+++ src/app/components/dropdown.js
@@ -1,31 +1,19 @@
-export default () => {
-    const button = document.querySelector('.js-dropMenu');
-    const menu = document.querySelector('.js-dropdown-content');
-    button.addEventListener('click', () => {
-        if (menu.style.display === 'flex') {
-            menu.style.display = 'none';
-        } else {
-            menu.style.display = 'flex';
-        }
+export default class DropMenu {
+    constructor(container) {
+        this.container = container;
+        this.dropBtn = this.container.querySelector('.js-dropMenu');
+        this.menu = this.container.querySelector('.js-dropdown-content');
+
+        this.toggle();
     }
-    );
-};
 
-// export default class DropMenu {
-//     constructor(container) {
-//         this.container = container;
-//         const dropBtn = container.querySelector('.dropMenu');
-//         console.log(dropBtn);
-
-//         this.dropBtn.addEventListener('click', this.massege);
-//     }
-
-//     massege() {
-//         const dropMenu = container.querySelector('.dropdown-content');
-//         if (this.dropMenu.style.display === 'flex') {
-//             this.dropMenu.style.display = 'none';
-//         } else {
-//             this.dropMenu.style.display = 'flex';
-//         }
-//     }
-// }
+    toggle() {
+        this.dropBtn.addEventListener('click', () => {
+            if (this.menu.classList.contains('active')) {
+                this.menu.classList.remove('active');
+            } else {
+                this.menu.classList.add('active');
+            }
+        });
+    }
+}
