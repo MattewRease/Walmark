4d70a644f9a0e52db50f33fff214ea816464d700
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-31 12:44:28 +0100

    fixed mistakes

--- src/app/components/reminder.js
+++ src/app/components/reminder.js
@@ -10,32 +10,26 @@ export default class Reminder {
         this.time = this.container.querySelector('.js-selectTime');
         this.setBtn = this.container.querySelector('.js-setReminder');
         this.setBtn.disabled = true;
-        this.SELECTEDDAY = 'Select day';
-        this.SELECTEDTIME = 'Select time';
+        this.selected_day = this.day.value;
+        this.selected_time = this.time.value;
 
-        this.day.addEventListener('change', this.selectDay);
-        this.time.addEventListener('change', this.selectTime);
+        this.day.addEventListener('change', this.selectRemindTime);
+        this.time.addEventListener('change', this.selectRemindTime);
         this.setBtn.addEventListener('click', this.setReminder);
     }
 
-    selectDay = (event) => {
+    selectRemindTime = (event) => {
         const selectEl = event.target;
-        this.SELECTEDDAY = selectEl.value;
 
-        (this.SELECTEDDAY !== 'Select day') && (this.SELECTEDTIME !== 'Select time') ? this.setBtn.disabled = false : this.setBtn.disabled = true;
-    }
+        selectEl === this.day ? this.selected_day = selectEl.value : this.selected_time = selectEl.value;
 
-    selectTime = (event) => {
-        const selectEl = event.target;
-        this.SELECTEDTIME = selectEl.value;
-
-        (this.SELECTEDDAY !== 'Select day') && (this.SELECTEDTIME !== 'Select time') ? this.setBtn.disabled = false : this.setBtn.disabled = true;
+        (this.selected_day !== null && this.selected_day !== 'Select day') && (this.selected_time !== null && this.selected_time !== 'Select time') ? this.setBtn.disabled = false : this.setBtn.disabled = true;
     }
 
     setReminder = () => {
         const remind = {
-            day: this.SELECTEDDAY,
-            time: this.SELECTEDTIME
+            day: this.selected_day,
+            time: this.selected_time
         };
 
         axios({
