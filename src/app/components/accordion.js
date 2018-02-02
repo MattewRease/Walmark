c0984001e3b0bb1968faea2f2ebd01028323bb62
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-02 15:44:21 +0100

    fixed accordion

--- src/app/components/accordion.js
+++ src/app/components/accordion.js
@@ -1,26 +1,33 @@
 export default class Accordion {
     constructor(container) {
         this.container = container;
-        this.titles = this.container.querySelectorAll('.accordiontitle ');
-        this.select();
-    }
+        this.titles = this.container.querySelectorAll('.js-accordionTitle ');
 
-    select = () => {
-        this.titles.forEach(title => {
+        /**
+         * Event listener on selected title
+         */
+        this.titles.forEach((title => {
             title.addEventListener('click', () => {
                 this.toggle(title);
             });
-        });
+        }));
     }
+
+    /**
+     * Toggle accordion panels.
+     *
+     * When selected panel is open others are closed.
+     * @param {HTMLElement} title - selected panels title
+     */
 
     toggle = (title) => {
         if (!title.classList.contains('selected')) {
             this.titles.forEach(title => {
                 title.querySelector('.footer__dropdown-icon').classList.remove('up');
-                title.lastElementChild.classList.remove('active');
+                title.lastElementChild.classList.remove('active'); // Disable panels
             });
             title.querySelector('.footer__dropdown-icon').classList.add('up');
-            title.lastElementChild.classList.add('active');
+            title.lastElementChild.classList.add('active'); // Enable panel
         }
     }
 }
