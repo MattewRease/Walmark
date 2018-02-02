8d2651f32041a04a6a87f5434707ba5cd788a249
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-02 16:28:31 +0100

    renamed variables

--- src/app/components/history.js
+++ src/app/components/history.js
@@ -3,16 +3,16 @@ import countProgress from '../base/countProgress';
 export default class History {
     constructor(container) {
         this.container = container;
-        this.content = this.container.querySelector('.history-modal__content');
+        this.modalContent = this.container.querySelector('.history-modal__content');
         this.healthIndex = document.querySelector('.js-health'); // select health index component
         this.comparison = document.querySelector('.js-comparison'); // select comparison component
         this.copyIndexItem = document.querySelector('.js-health').cloneNode(true); // copy health index component
         this.copyComparisonItem = document.querySelector('.js-comparison').cloneNode(true); // copy comparison component
-        this.newIndexItem = this.content.appendChild(this.copyIndexItem, this.healthIndex.nextSibling); // insert index component to modal window
-        this.newComparisonItem = this.content.appendChild(this.copyComparisonItem, this.comparison.nextSibling); // insert comparison component to modal window
-        this.firstBtn = document.querySelector('.js-firstHistory');
-        this.closeBtn = this.container.querySelector('.history-modal__close');
-        this.secondBtn = document.querySelector('.js-secondHistory');
+        this.newIndexItem = this.modalContent.appendChild(this.copyIndexItem, this.healthIndex.nextSibling); // insert index component to modal window
+        this.newComparisonItem = this.modalContent.appendChild(this.copyComparisonItem, this.comparison.nextSibling); // insert comparison component to modal window
+        this.historyFirstBtn = document.querySelector('.js-firstHistory');
+        this.modalCloseButton = this.container.querySelector('.history-modal__close');
+        this.historySecondButton = document.querySelector('.js-secondHistory');
         this.modalWindow = document.querySelector('.js-history-modal');
         this.indexRate = this.container.querySelector('.js-numberIndex');
 
@@ -22,11 +22,11 @@ export default class History {
     }
 
     callModal = () => {
-        this.secondBtn.addEventListener('click', this.secondBtn);
+        this.historySecondButton.addEventListener('click', this.historySecondButton);
     }
 
     firstHistory = () => {
-        this.firstBtn.addEventListener('click', () => {
+        this.historyFirstBtn.addEventListener('click', () => {
             this.container.classList.add('active');
             this.indexRate.dataset.index = 28;
             const setRate = this.indexRate.dataset.index;
@@ -35,7 +35,7 @@ export default class History {
     }
 
     secondHistory = () => {
-        this.secondBtn.addEventListener('click', () => {
+        this.historySecondButton.addEventListener('click', () => {
             this.container.classList.add('active');
             this.indexRate.dataset.index = 87;
             const setRate = this.indexRate.dataset.index;
@@ -44,8 +44,8 @@ export default class History {
     }
 
     historyModalWindow = () => {
-        this.closeBtn.addEventListener('click', () => {
-            this.closeBtn ? this.close() : false;
+        this.modalCloseButton.addEventListener('click', () => {
+            this.modalCloseButton ? this.close() : false;
         });
         this.modalWindow.addEventListener('click', () => {
             event.target === this.modalWindow ? this.close() : false;
