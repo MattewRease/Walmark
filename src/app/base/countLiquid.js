c03e765cefad13a2f0b6abbb9fa947de018de424
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-04 20:05:43 +0100

    component for count liquid rate

---
+++ src/app/base/countLiquid.js
@@ -0,0 +1,24 @@
+const countLiquid = (container, setLitresData, setOptimalLitresData) => {
+    const liquidRate = container.querySelector('.js-liquidRate');
+    const liquidLowIcon = container.querySelector('.js-liquidLow');
+    const liquidNormalIcon = container.querySelector('.js-liquidNormal');
+    const optimalLitres = container.querySelector('.js-liquid-optimal');
+
+    optimalLitres.innerHTML = setOptimalLitresData;
+
+    if (setLitresData < setOptimalLitresData) {
+        liquidRate.innerHTML = 'insufficient';
+        if (liquidRate.classList.contains('range--hight')) liquidRate.classList.remove('range--hight');
+        liquidRate.classList.add('range--low');
+        if (liquidNormalIcon.classList.contains('active')) liquidNormalIcon.classList.remove('active');
+        liquidLowIcon.classList.add('active');
+    } else {
+        liquidRate.innerHTML = 'sufficient';
+        if (liquidRate.classList.contains('range--low')) liquidRate.classList.remove('range--low');
+        liquidRate.classList.add('range--hight');
+        if (liquidLowIcon.classList.contains('active')) liquidLowIcon.classList.remove('active');
+        liquidNormalIcon.classList.add('active');
+    }
+};
+
+export default countLiquid;
